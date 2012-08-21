c3e56aa26f185ddaf8598514b1fd9175a2d840d9
Tõnis Tiigi <tonistiigi@gmail.com>
2012-08-21 17:53:59 +0300

    Starting sample

---
+++ sample/sample.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var app = express()
+
+
+var ipizza = require('ipizza')
+
+ipizza.provider('swedbank',
+  { clientId: 'uid202196'
+  , privateKey: __dirname + '/keys/swedbank.key.pem'
+  , certificate: __dirname + '/keys/swedbank.cert.pem'
+  , url: 'https://pangalink.net/banklink/008/swedbank'
+  })
+
+
+
+app.get('/', function (req, res) {
+  var data = { provider: 'swedbank'
+             , amount: 19
+             , id: 1234
+             , ref: 121312952
+             , msg: 'goods'
+             }
+
+  var params = ipizza.payment(data).json()
+  res.set('Content-Type', 'text/html')
+  res.write('<form action="https://pangalink.net/banklink/008/swedbank" method="post"><input type="submit">');
+  for (var i in params) {
+    res.write('<input type="text" name="'+i+'" value="'+params[i]+'">');
+  }
+  res.write('</form>');
+  res.end();
+})
+
+app.listen(4000)
