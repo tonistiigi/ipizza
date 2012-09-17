deb25be1f80a14b27fe96b5c45e18abde6a29a10
Tõnis Tiigi <tonistiigi@gmail.com>
2012-09-17 22:54:47 +0300

    Add CasperJS Travis conf

--- casperjs/test.js
+++ casperjs/test.js
@@ -1,4 +1,8 @@
-var casper = require('casper').create()
+var casper = require('casper').create({
+  pageSettings: {
+    webSecurityEnabled: false
+  }
+})
 
 casper.start('http://127.0.0.1:4000/')
 
