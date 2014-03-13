40da69eb95313f754562636818da34001f73382d
Tonis Tiigi <tonistiigi@gmail.com>
2014-03-14 00:38:07 +0200

    Krediidipank pack length calculation fix

--- providers/ipizzabank.js
+++ providers/ipizzabank.js
@@ -249,7 +249,7 @@ IpizzaBank.prototype.json = function () {
 IpizzaBank.prototype.genPackage_ = function (params) {
   return this.lastPackage_ = _.reduce(params, function (memo, val, key) {
     val = val.toString()
-    var len = (~['seb', 'lhv'].indexOf(this.name)) && this.utf8_ ?
+    var len = (~['seb', 'lhv', 'krediidipank'].indexOf(this.name)) && this.utf8_ ?
       Buffer.byteLength(val, 'utf8') : val.length
     memo += S('0').repeat(3 - len.toString().length).toString()
       + len + val
